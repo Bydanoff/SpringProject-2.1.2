3638d97a468a7eb1b2bad9f09292a34aae09a170
Bydanoff <erzhan.vst@gmail.com>
2024-11-17 20:38:26 +0300

    First commit.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHW1.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println((cat1 == cat2) ? "cat и cat2 имеют одинаковые ссылки" : "cat и cat2 имеют разные ссылки");
+        System.out.println((beanHW1 == beanHW2) ? "beanHW и beanHW2 имеют одинаковые ссылки" : "beanHW1 и beanHW2 имеют разные ссылки");
     }
 }
